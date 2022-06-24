38937605a40a2c7ea6f535c180d2c15ebcb3b9a2
Hilrick Louw <hilricklouw@hotmail.com>
2022-06-24 13:19:52 +1200

    Fresh Repo submistion of RPS. I'm stuck on getting the order right on what beats what. Otherwise it seems to be working

---
+++ script.js
@@ -0,0 +1,38 @@
+const choices = ["ROCK", "PAPER", "SCISSORS",];
+const rock = "ROCK";
+const paper = "PAPER";
+const scissors = "SCISSORS";
+
+"ROCK" > "SCISSORS";
+"SCISSORS" > "PAPER";
+"PAPER" > "ROCK";
+
+//const playerSelection = userChoice;
+//const computerSelection = computerPlay();
+
+function computerPlay(){
+    const randomChoice = Math.floor((Math.random() * 3 ));
+    return choices[randomChoice];
+};
+//console.log(computerPlay());
+
+function playerSelection(){
+    let yourChoice = prompt ("Choose Rock, Paper or Scissors").toUpperCase();
+    return yourChoice;
+}
+//console.log(playerSelection());
+
+function playRound(playerSelection, computerPlay){
+    if (playerSelection > computerPlay){
+        console.log("You win! " + playerSelection + " beats " + computerPlay + ".");
+    } else if (playerSelection < computerPlay){
+        console.log("You lose! " + computerPlay + " beats " + playerSelection + ".");
+    } else {
+        console.log("It's a draw. Both chose " + playerSelection + ".");
+    }
+}
+   
+
+console.log(playRound(playerSelection(), computerPlay()));
+//console.log(playRound());
+//console.log(playRound());
