cd14249134b30df7354898c25bdd931ca6730208
Hilrick Louw <hilricklouw@hotmail.com>
2022-06-26 12:02:10 +1200

    paper seems to beat everything and is always being selected. Really close.

--- script.js
+++ script.js
@@ -1,50 +1,33 @@
 const choices = ["ROCK", "PAPER", "SCISSORS",];
-/*const rock = "ROCK";
-const paper = "PAPER";
-const scissors = "SCISSORS";*/
 
-/*"ROCK" > "SCISSORS";
-"SCISSORS" > "PAPER";
-"PAPER" > "ROCK";*/
-
-const playerChoice = playerSelection();
+let playerSelection = prompt ("Choose Rock, Paper or Scissors").toUpperCase();
 const computerSelection = computerPlay();
 
 function computerPlay(){
     const randomChoice = Math.floor((Math.random() * 3 ));
     return choices[randomChoice];
-};
-//console.log(computerPlay());
-
-function playerSelection(){
-    let yourChoice = prompt ("Choose Rock, Paper or Scissors").toUpperCase();
-    return yourChoice;
 }
-//console.log(playerSelection());
 
-function playRound(playerChoice, computerSelection){
-    if (playerChoice == "ROCK", computerSelection == "SCISSORS") {
-        return ("You win! " + playerChoice + " beats " + computerSelection + ".");
-    } else if (playerChoice == "ROCK", computerSelection == "PAPER") {
-        console.log("You lose! " + computerSelection + " beats " + playerChoice + ".");
-    } else if (playerChoice == "ROCK", computerSelection == "ROCK") {
-        console.log("It's a draw. Both chose " + playerChoice + ".");
-    } else if (playerChoice == "PAPER", computerSelection == "PAPER"){
-        console.log("It's a draw. Both chose " + playerChoice + ".");
-    } else if (playerChoice == "PAPER", computerSelection == "ROCK"){
-        console.log("You win! " + playerChoice + " beats " + computerSelection + ".");
-    } else if (playerChoice == "PAPER", computerSelection == "SCISSORS"){
-        console.log("You lose! " + computerSelection + " beats " + playerChoice + ".");
-    } else if (playerChoice == "SCISSORS", computerSelection == "SCISSORS"){
-        console.log("It's a draw. Both chose " + playerChoice + ".");
-    } else if (playerChoice == "SCISSORS", computerSelection == "PAPER"){
-        console.log("You win! " + playerChoice + " beats " + computerSelection + ".");
+function playRound(playerSelection, computerSelection){
+    if (playerSelection == "ROCK", computerSelection == "ROCK") {
+        return ("It's a draw. Both chose " + playerSelection + ".");
+    } else if (playerSelection == "ROCK", computerSelection == "PAPER") {
+        return ("You lose! " + computerSelection + " beats " + playerSelection + ".");
+    } else if (playerSelection == "ROCK", computerSelection == "SCISSORS") {
+        return ("You win! " + playerSelection + " beats " + computerSelection + ".");
+    } else if (playerSelection == "PAPER", computerSelection == "PAPER"){
+        return ("It's a draw. Both chose " + playerSelection + ".");
+    } else if (playerSelection == "PAPER", computerSelection == "ROCK"){
+        return ("You win! " + playerSelection + " beats " + computerSelection + ".");
+    } else if (playerSelection == "PAPER", computerSelection == "SCISSORS"){
+        return ("You lose! " + computerSelection + " beats " + playerSelection + ".");
+    } else if (playerSelection == "SCISSORS", computerSelection == "SCISSORS"){
+        return ("It's a draw. Both chose " + playerSelection + ".");
+    } else if (playerSelection == "SCISSORS", computerSelection == "PAPER"){
+        return ("You win! " + playerSelection + " beats " + computerSelection + ".");
     } else {
-        console.log("You lose! " + computerSelection + " beats " + playerChoice + ".");
+        return ("You lose! " + computerSelection + " beats " + playerSelection + ".");
     }
-}
-   
+} 
 
-console.log(playRound(playerChoice, computerSelection));
-//console.log(playRound());
-//console.log(playRound());
+console.log(playRound(playerSelection, computerSelection));
