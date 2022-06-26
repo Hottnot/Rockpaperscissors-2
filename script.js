abfdf8e688f0e44f2316ce7c95f21a482d7a789e
Hilrick Louw <hilricklouw@hotmail.com>
2022-06-26 23:33:45 +1200

    fix!!! Need to maybe think about adding a break in the game? Or not!

--- script.js
+++ script.js
@@ -1,36 +1,50 @@
-const choices = ["ROCK", "PAPER", "SCISSORS",];
-
-
-const computerSelection = computerPlay();
-const playerSelection = prompt ("Choose Rock, Paper or Scissors").toUpperCase();
-//console.log(playerSelection);
+// this let's computer do a random choice.
 
 function computerPlay(){
     const randomChoice = Math.floor((Math.random() * 3 ));
     return choices[randomChoice];
 }
 
-//console.log(computerPlay());
+// This function playes a single round and declares a winner with points for each player
 
 function playRound(playerSelection, computerSelection){
-   // 
-    //const playerSelection = prompt ("Choose Rock, Paper or Scissors").toUpperCase();
     if (playerSelection === computerSelection) {
         return ("It's a draw. Both chose " + playerSelection + ".");
-    } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
-        return ("You lose! " + computerSelection + " beats " + playerSelection + ".");
+    } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER"){
+        playerScore++;
+        return ("You win! " + playerSelection + " beats " + computerSelection + ".");
     } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+        playerScore++;
         return ("You win! " + playerSelection + " beats " + computerSelection + ".");
     } else if (playerSelection === "PAPER" && computerSelection === "ROCK"){
+        playerScore++;
         return ("You win! " + playerSelection + " beats " + computerSelection + ".");
     } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS"){
+        computerScore++;
         return ("You lose! " + computerSelection + " beats " + playerSelection + ".");
-    } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER"){
-        return ("You win! " + playerSelection + " beats " + computerSelection + ".");
+    } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+        computerScore++;
+        return ("You lose! " + computerSelection + " beats " + playerSelection + ".");
     } else {
+        computerScore++;
         return ("You lose! " + computerSelection + " beats " + playerSelection + ".");
-    }
+    }   
 } 
 
-console.log(playRound(playerSelection, computerSelection));
+//These are my declared variables.
+
+const choices = ["ROCK", "PAPER", "SCISSORS",];
+let playerScore = parseInt(0);
+let computerScore = parseInt(0);
+
+//This makes sure we only play 5 rounds and show's scores
+
+for(var i=0;i<5;i++){
+    const computerSelection = computerPlay();
+    let playerSelection = prompt ("Choose Rock, Paper or Scissors").toUpperCase();
+    console.log(playRound(playerSelection, computerSelection));
+    console.log("your score = " + playerScore);
+    console.log("Computer's score = " + computerScore);
+}
+
 
